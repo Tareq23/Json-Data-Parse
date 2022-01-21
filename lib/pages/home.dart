b5f54dbeb3740088d3614d5ad63d8f7fef85697c
Tareq23 <mdtarequlislam634@gmail.com>
2022-01-21 11:18:34 +0600

    add fake json data

---
+++ lib/pages/home.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff123456),
+        title: const Text('Json Data',style: TextStyle(fontSize: 32,fontWeight: FontWeight.w700,color: Colors.white),),
+      ),
+    );
+  }
+}
